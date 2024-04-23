d6fd90aa9f1d870524d345edb5bc971823b90c4b
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-04-24 00:29:46 +0200

    feature: download each package json file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,11 +9,93 @@ import (
 )
 
 func main() {
-	coreFormulasFile := "core_formulas.txt"
+	coreFormulasFile := "core_formulas.json"
 
 	if fileDoNotExist(coreFormulasFile) {
 		getCoreFormulas(coreFormulasFile)
 	}
+
+	formulas_list, err := getFormulasFromFile(coreFormulasFile)
+	if err != nil {
+		fmt.Println("Error getting formulas list: ", err)
+	}
+
+	for _, f := range formulas_list {
+		getFormulaInfo(f)
+	}
+}
+
+type Formula struct {
+	Name              string                 `json:"name"`
+	FullName          string                 `json:"full_name"`
+	Tap               string                 `json:"tap"`
+	Oldname           interface{}            `json:"oldname"` // Can be null
+	Oldnames          []string               `json:"oldnames"`
+	Aliases           []string               `json:"aliases"`
+	VersionedFormulae []string               `json:"versioned_formulae"`
+	Desc              string                 `json:"desc"`
+	License           string                 `json:"license"`
+	Homepage          string                 `json:"homepage"`
+	Versions          map[string]interface{} `json:"versions"`
+	Urls              map[string]interface{} `json:"urls"`
+	Revision          int                    `json:"revision"`
+	VersionScheme     int                    `json:"version_scheme"`
+	Bottle            map[string]interface{} `json:"bottle"`
+	// ... other fields (pour_bottle_only_if, keg_only, etc.) are omitted for brevity
+}
+
+func getFormulasFromFile(fileName string) ([]string, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return []string{}, err
+	}
+
+	var formulas []Formula
+	err = json.Unmarshal(data, &formulas)
+	if err != nil {
+		fmt.Println("Error parsing JSON: ", err)
+		return []string{}, err
+	}
+
+	var allFormulas []string
+	for _, formula := range formulas {
+		allFormulas = append(allFormulas, formula.Name)
+	}
+
+	return allFormulas, nil
+}
+
+func getFormulaInfo(f string) {
+	filePath := "./formulas/" + f + ".json"
+	url := "https://formulae.brew.sh/api/formula/" + f + ".json"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error: can not reach API endpoint", err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	outFile, err := os.Create(filePath)
+
+	if err != nil {
+		fmt.Println("Error creating file: ", err.Error())
+		return
+	}
+
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, resp.Body)
+
+	if err != nil {
+		fmt.Println("Error writing to a file: ", err.Error())
+		return
+	}
+
+	fmt.Println("successfully written JSON data into ", filePath)
 }
 
 func fileDoNotExist(fileName string) bool {
