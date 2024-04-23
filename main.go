d322000ae5c4bd8dfc7671369affac16df43a310
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-04-24 01:01:08 +0200

    feature: more struct items

--- main.go
+++ main.go
@@ -28,22 +28,53 @@ func main() {
 }
 
 type Formula struct {
-	Name              string                 `json:"name"`
-	FullName          string                 `json:"full_name"`
-	Tap               string                 `json:"tap"`
-	Oldname           interface{}            `json:"oldname"` // Can be null
-	Oldnames          []string               `json:"oldnames"`
-	Aliases           []string               `json:"aliases"`
-	VersionedFormulae []string               `json:"versioned_formulae"`
-	Desc              string                 `json:"desc"`
-	License           string                 `json:"license"`
-	Homepage          string                 `json:"homepage"`
-	Versions          map[string]interface{} `json:"versions"`
-	Urls              map[string]interface{} `json:"urls"`
-	Revision          int                    `json:"revision"`
-	VersionScheme     int                    `json:"version_scheme"`
-	Bottle            map[string]interface{} `json:"bottle"`
-	// ... other fields (pour_bottle_only_if, keg_only, etc.) are omitted for brevity
+	Name                    string                 `json:"name"`
+	FullName                string                 `json:"full_name"`
+	Tap                     string                 `json:"tap"`
+	Oldname                 interface{}            `json:"oldname"` // Can be null
+	Oldnames                []string               `json:"oldnames"`
+	Aliases                 []string               `json:"aliases"`
+	VersionedFormulae       []string               `json:"versioned_formulae"`
+	Desc                    string                 `json:"desc"`
+	License                 string                 `json:"license"`
+	Homepage                string                 `json:"homepage"`
+	Versions                map[string]interface{} `json:"versions"`
+	Urls                    map[string]interface{} `json:"urls"`
+	Revision                int                    `json:"revision"`
+	VersionScheme           int                    `json:"version_scheme"`
+	Bottle                  map[string]interface{} `json:"bottle"`
+	PourBottleOnlyIf        interface{}            `json:"pour_bottle_only_if"`
+	KeyOnly                 bool                   `json:"keg_only"`
+	KegOnlyReason           interface{}            `json:"keg_only_reason"`
+	Options                 []string               `json:"options"`
+	BuildDependencies       []string               `json:"build_dependencies"`
+	Dependencies            []string               `json:"dependencies"`             // most important
+	TestDependencies        []string               `json:"test_dependencies"`        // most important
+	RecommendedDependencies []string               `json:"recommended_dependencies"` // most important
+	OptionalDependencies    []string               `json:"optional_dependencies"`    // most important
+	UsesFromMacos           []string               `json:"uses_from_macos"`          // most important
+	UsesFromMacosBounds     []string               `json:"uses_from_macos_bounds"`   // most important
+	Requirements            []string               `json:"requirements"`             // most important
+	ConflictsWith           []string               `json:"conflicts_with"`           // most important
+	ConflictsWithReasons    []string               `json:"conflicts_with_reasons"`   // most important
+	LinkOverwrite           []string               `json:"link_overwrite"`           // most important
+	Caveats                 interface{}            `json:"caveats"`
+	Installed               []string               `json:"installed"`
+	LinkedKeg               interface{}            `json:"linked_keg"`
+	Pinned                  bool                   `json:"pinned"`
+	Outdated                bool                   `json:"outdated"`
+	Deprecated              bool                   `json:"deprecated"`
+	DeprecationDate         interface{}            `json:"deprecation_date"`
+	DeprecationReason       interface{}            `json:"deprecation_reason"`
+	Disabled                bool                   `json:"disabled"`
+	DisableDate             interface{}            `json:"disable_date"`
+	DisableReason           interface{}            `json:"disable_reason"`
+	PostInstallDefined      bool                   `json:"post_install_defined"`
+	Service                 interface{}            `json:"service"`
+	TapGitHead              string                 `json:"tap_git_head"`
+	RubySourcePath          string                 `json:"ruby_source_path"`
+	RubySourceChecksum      []string               `json:"ruby_source_checksum"`
+	Variations              []string               `json:"variations"`
 }
 
 func getFormulasFromFile(fileName string) ([]string, error) {
