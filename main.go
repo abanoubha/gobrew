53238991de7f7dd975c1aa341246cc6844ab6cfa
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-05-16 07:08:09 +0300

    perf: comment out unwanted fields; to optimize space and gc

--- main.go
+++ main.go
@@ -75,53 +75,53 @@ func getPackageCount(fileName, lang string) {
 }
 
 type Formula struct {
-	Name                    string                 `json:"name"`
-	FullName                string                 `json:"full_name"`
-	Tap                     string                 `json:"tap"`
-	Oldname                 interface{}            `json:"oldname"`
-	Oldnames                []string               `json:"oldnames"`
-	Aliases                 []string               `json:"aliases"`
-	VersionedFormulae       []string               `json:"versioned_formulae"`
-	Desc                    string                 `json:"desc"`
-	License                 string                 `json:"license"`
-	Homepage                string                 `json:"homepage"`
-	Versions                map[string]interface{} `json:"versions"`
-	Urls                    map[string]interface{} `json:"urls"`
-	Revision                int                    `json:"revision"`
-	VersionScheme           int                    `json:"version_scheme"`
-	Bottle                  map[string]interface{} `json:"bottle"`
-	PourBottleOnlyIf        interface{}            `json:"pour_bottle_only_if"`
-	KeyOnly                 bool                   `json:"keg_only"`
-	KegOnlyReason           interface{}            `json:"keg_only_reason"`
-	Options                 []string               `json:"options"`
-	BuildDependencies       []string               `json:"build_dependencies"`
-	Dependencies            []string               `json:"dependencies"`
-	TestDependencies        []string               `json:"test_dependencies"`
-	RecommendedDependencies []string               `json:"recommended_dependencies"`
-	OptionalDependencies    []string               `json:"optional_dependencies"`
-	UsesFromMacos           interface{}            `json:"uses_from_macos"`
-	UsesFromMacosBounds     interface{}            `json:"uses_from_macos_bounds"`
-	Requirements            interface{}            `json:"requirements"`
-	ConflictsWith           []string               `json:"conflicts_with"`
-	ConflictsWithReasons    []string               `json:"conflicts_with_reasons"`
-	LinkOverwrite           []string               `json:"link_overwrite"`
-	Caveats                 interface{}            `json:"caveats"`
-	Installed               interface{}            `json:"installed"`
-	LinkedKeg               interface{}            `json:"linked_keg"`
-	Pinned                  bool                   `json:"pinned"`
-	Outdated                bool                   `json:"outdated"`
-	Deprecated              bool                   `json:"deprecated"`
-	DeprecationDate         interface{}            `json:"deprecation_date"`
-	DeprecationReason       interface{}            `json:"deprecation_reason"`
-	Disabled                bool                   `json:"disabled"`
-	DisableDate             interface{}            `json:"disable_date"`
-	DisableReason           interface{}            `json:"disable_reason"`
-	PostInstallDefined      bool                   `json:"post_install_defined"`
-	Service                 interface{}            `json:"service"`
-	TapGitHead              string                 `json:"tap_git_head"`
-	RubySourcePath          string                 `json:"ruby_source_path"`
-	RubySourceChecksum      interface{}            `json:"ruby_source_checksum"`
-	Variations              interface{}            `json:"variations"`
+	Name                    string      `json:"name"`
+	BuildDependencies       []string    `json:"build_dependencies"`
+	Dependencies            []string    `json:"dependencies"`
+	TestDependencies        []string    `json:"test_dependencies"`
+	RecommendedDependencies []string    `json:"recommended_dependencies"`
+	OptionalDependencies    []string    `json:"optional_dependencies"`
+	Requirements            interface{} `json:"requirements"`
+	// FullName                string      `json:"full_name"`
+	// UsesFromMacos           interface{} `json:"uses_from_macos"`
+	// UsesFromMacosBounds     interface{} `json:"uses_from_macos_bounds"`
+	// Tap                     string      `json:"tap"`
+	// Oldname                 interface{}            `json:"oldname"`
+	// Oldnames                []string               `json:"oldnames"`
+	// Aliases                 []string               `json:"aliases"`
+	// VersionedFormulae       []string               `json:"versioned_formulae"`
+	// Desc                    string                 `json:"desc"`
+	// License                 string                 `json:"license"`
+	// Homepage                string                 `json:"homepage"`
+	// Versions                map[string]interface{} `json:"versions"`
+	// Urls                    map[string]interface{} `json:"urls"`
+	// Revision                int                    `json:"revision"`
+	// VersionScheme           int                    `json:"version_scheme"`
+	// Bottle                  map[string]interface{} `json:"bottle"`
+	// PourBottleOnlyIf        interface{}            `json:"pour_bottle_only_if"`
+	// KeyOnly                 bool                   `json:"keg_only"`
+	// KegOnlyReason           interface{}            `json:"keg_only_reason"`
+	// Options                 []string               `json:"options"`
+	// ConflictsWith           []string               `json:"conflicts_with"`
+	// ConflictsWithReasons    []string               `json:"conflicts_with_reasons"`
+	// LinkOverwrite           []string               `json:"link_overwrite"`
+	// Caveats                 interface{}            `json:"caveats"`
+	// Installed               interface{}            `json:"installed"`
+	// LinkedKeg               interface{}            `json:"linked_keg"`
+	// Pinned                  bool                   `json:"pinned"`
+	// Outdated                bool                   `json:"outdated"`
+	// Deprecated              bool                   `json:"deprecated"`
+	// DeprecationDate         interface{}            `json:"deprecation_date"`
+	// DeprecationReason       interface{}            `json:"deprecation_reason"`
+	// Disabled                bool                   `json:"disabled"`
+	// DisableDate             interface{}            `json:"disable_date"`
+	// DisableReason           interface{}            `json:"disable_reason"`
+	// PostInstallDefined      bool                   `json:"post_install_defined"`
+	// Service                 interface{}            `json:"service"`
+	// TapGitHead              string                 `json:"tap_git_head"`
+	// RubySourcePath          string                 `json:"ruby_source_path"`
+	// RubySourceChecksum      interface{}            `json:"ruby_source_checksum"`
+	// Variations              interface{}            `json:"variations"`
 }
 
 func getFormulasFromFile(fileName, langName string) (map[interface{}]struct{}, error) {
